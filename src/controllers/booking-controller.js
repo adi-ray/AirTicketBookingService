9919bb2a15c9f49e24e3f949686e0ff57679fe58
adi-ray <adityadns03@gmail.com>
2025-05-23 01:50:10 +0530

    Completed create booking

---
+++ src/controllers/booking-controller.js
@@ -0,0 +1,28 @@
+const { StatusCodes } = require("http-status-codes");
+
+const { BookingService } = require("../services/index");
+
+const bookingService = new BookingService();
+
+const create = async (req, res) => {
+  try {
+    const response = await bookingService.createBooking(req.body);
+    return res.status(StatusCodes.OK).json({
+      data: response,
+      success: true,
+      message: "Booking completed successfully",
+      err: {},
+    });
+  } catch (error) {
+    return res.status(error.statusCode).json({
+      data: {},
+      success: false,
+      message: error.message,
+      err: error.explanation,
+    });
+  }
+};
+
+module.exports = {
+  create,
+};
--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -20,6 +20,24 @@ class BookingRepository {
       );
     }
   }
+
+  async update(bookingId, data) {
+    try {
+      const booking = await Booking.findByPk(bookingId);
+      if (data.status) {
+        booking.status = data.status;
+      }
+      await booking.save();
+      return booking;
+    } catch (error) {
+      throw new AppError(
+        "RepositoryError",
+        "Error updating booking",
+        "There was an error updating the booking. Please try again.",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
 }
 
 module.exports = BookingRepository;
