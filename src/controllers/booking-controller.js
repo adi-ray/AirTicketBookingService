2bc4d6b06976b58aae2cdcbaa153f62059addf60
adi-ray <adityadns03@gmail.com>
2025-05-26 02:53:45 +0530

    Add RabbitMQ config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,27 +2,47 @@ const { StatusCodes } = require("http-status-codes");
 
 const { BookingService } = require("../services/index");
 
+const { createChannel } = require("../utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
+
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
-    return res.status(StatusCodes.OK).json({
-      data: response,
-      success: true,
-      message: "Booking completed successfully",
-      err: {},
-    });
-  } catch (error) {
-    return res.status(error.statusCode).json({
-      data: {},
-      success: false,
-      message: error.message,
-      err: error.explanation,
+class BookingController {
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const payload = {
+      data: {
+        subject: "This is a notification from queue",
+        content: "Some queue will subscribe this",
+        recepientEmail: "adityadns03@gmail.com",
+        notificationTime: "2025-05-26T02:49:00",
+      },
+      service: "CREATE_TICKET",
+    };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
+    return res.status(200).json({
+      message: "Succesfully published the event",
     });
   }
-};
 
-module.exports = {
-  create,
-};
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      return res.status(StatusCodes.OK).json({
+        data: response,
+        success: true,
+        message: "Booking completed successfully",
+        err: {},
+      });
+    } catch (error) {
+      return res.status(error.statusCode).json({
+        data: {},
+        success: false,
+        message: error.message,
+        err: error.explanation,
+      });
+    }
+  }
+}
+
+module.exports = BookingController;
